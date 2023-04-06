d11dae76696f631eb29de59bb1aa0d37e3da18a4
Whyiest <74537743+Whyiest@users.noreply.github.com>
2023-04-06 10:54:17 +0200

    Migrate to MySQL

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Database myDb = new Database("projetece.database.windows.net:1433", "SwiftChat", "swiftchat@projetece", "Ines123#");
+        Database myDb = new Database("swiftchatserver.mysql.database.azure.com", "swiftchatdb", "siwftchat", "Ines123#");
         myDb.connect();
         myDb.disconnect();
 
--- src/modele/Database.java
+++ src/modele/Database.java
@@ -17,51 +17,28 @@ public class Database {
     }
 
     public void connect() {
-        String connectionUrl = String.format("jdbc:sqlserver://%s;database=%s;user=%s;password=%s;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;", server, database, username, password);
+
         try {
-            connection = DriverManager.getConnection(connectionUrl);
-            System.out.println("Connected to database");
-        } catch (SQLException e) {
-            System.out.println("Failed to connect to database: " + e.getMessage());
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            String url = "jdbc:mysql://" + server + "/" + database + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+            System.out.println(url);
+            connection = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection to database " + database + " successful.");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Connection to database " + database + " failed.");
+            e.printStackTrace();
         }
     }
 
     public void disconnect() {
         try {
             connection.close();
-            System.out.println("Disconnected from database");
+            System.out.println("Disconnection from database " + database + " successful.");
         } catch (SQLException e) {
-            System.out.println("Failed to disconnect from database: " + e.getMessage());
+            System.out.println("Disconnection from database " + database + " failed.");
+            e.printStackTrace();
         }
     }
 
-    public void insert(String table, String[] columns, String[] values) {
-        String columnsString = String.join(", ", columns);
-        String valuesString = String.join(", ", values);
-        String query = String.format("INSERT INTO %s (%s) VALUES (%s)", table, columnsString, valuesString);
-        try {
-            Statement statement = connection.createStatement();
-            statement.executeUpdate(query);
-            System.out.println("Inserted into database");
-        } catch (SQLException e) {
-            System.out.println("Failed to insert into database: " + e.getMessage());
-        }
-    }
-
-    public void select(String table, String[] columns, String[] conditions) {
-        String columnsString = String.join(", ", columns);
-        String conditionsString = String.join(" AND ", conditions);
-        String query = String.format("SELECT %s FROM %s WHERE %s", columnsString, table, conditionsString);
-        try {
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(query);
-            System.out.println("Selected from database");
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(1));
-            }
-        } catch (SQLException e) {
-            System.out.println("Failed to select from database: " + e.getMessage());
-        }
-    }
 }
 
