e2214a98f31854f43f709cef1829b81bdce92bf0
curti <alexandre.curti@edu.ece.fr>
2023-04-12 16:43:35 +0200

    Login is working

--- src/client/view/LoginForm.java
+++ src/client/view/LoginForm.java
@@ -1,5 +1,6 @@
 package client.view;
 
+import client.Client;
 import client.clientModel.ResponseAnalyser;
 import client.clientModel.User;
 import client.controler.ServerConnection;
@@ -13,65 +14,76 @@ import java.awt.event.ActionListener;
 public class LoginForm extends JDialog {
 
     public User user;
-    private JTextField tfUsername;
-    private JPasswordField pfPassword;
+    private JTextField userNameField;
+    private JPasswordField passwordField;
     private JButton btnLogin;
     private JButton btnCancel;
     private JPanel loginForm;
     private JButton clickToRegisterAButton;
-    private ServerConnection serverConnection;
-    public  int windowID;
+    private final ServerConnection serverConnection;
 
     public LoginForm(JFrame parent, ServerConnection serverConnection) {
 
         super(parent);
 
-        // This windows is identified as 0
-        this.windowID = 0;
         this.serverConnection = serverConnection;
 
         // Create the form :
         setTitle("Login Form");
         setLocationRelativeTo(parent);
-        initForm();
-    }
-
-    public void initForm () {
-        // Create the form
         setContentPane(loginForm);
         setMinimumSize(new Dimension(700, 600));
         setModal(true);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         clickToRegisterAButton.setBorderPainted(false);
+
+        // FIELD TEXT
         btnLogin.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String username = tfUsername.getText();
-                String password = String.valueOf(pfPassword.getPassword());
+
+                String username = userNameField.getText();
+                String password = String.valueOf(passwordField.getPassword());
 
                 user = getAuthenticatedUser(username, password);
+
+                // If user password and ID are correct :
                 if (user != null) {
+                    // Set client to logged
+                    Client.setClientIsLogged(true);
+                    Client.setClientID(user.getId());
+                    ViewManagement.setCurrentDisplay(2);
                     dispose();
-                    windowID = 2; // we go to the contacts window
-                } else {
+                }
+                // Otherwise :
+                else {
                     JOptionPane.showMessageDialog(LoginForm.this, "Email or password Invalid", "Try again", JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
+
+        // CANCEL BUTTON
         btnCancel.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
             }
         });
+
+        // REGISTRATION BUTTON
         clickToRegisterAButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                windowID = 1; // We go to the registration form
-                dispose();
+                // Open the registration form
+                ViewManagement.setCurrentDisplay(1);
+
+                // Close the current window
+                closeLoginWindow();
             }
         });
     }
+
+
     public void closeLoginWindow () {
         setVisible(false);
         dispose();
@@ -79,9 +91,6 @@ public class LoginForm extends JDialog {
 
     public void openLoginWindow () {
         setVisible(true);
-    }
-    public int getWindowID() {
-        return windowID;
     }
 
     public User getAuthenticatedUser(String userName, String password) {
@@ -91,8 +100,8 @@ public class LoginForm extends JDialog {
         ResponseAnalyser responseAnalyser = new ResponseAnalyser(serverResponse);
         userLoggedID = responseAnalyser.login();
 
+        // If the login had success
         if (userLoggedID != -1) {
-
             serverResponse = serverConnection.getUserByID(userLoggedID);
             serverConnection.upDateLastConnectinTime(userLoggedID);
             serverConnection.changeStatus(userLoggedID, "ONLINE");
@@ -100,8 +109,13 @@ public class LoginForm extends JDialog {
             User loggedUser = responseAnalyserSecond.extractUser();
             return loggedUser;
         }
+        // If the login failed
         else {
             return null;
         }
     }
+
+
+
+
 }
--- src/client/view/ViewManagement.java
+++ src/client/view/ViewManagement.java
@@ -3,23 +3,24 @@ package client.view;
 import client.clientModel.User;
 import client.controler.ServerConnection;
 
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
 public class ViewManagement implements Runnable {
      public ServerConnection serverConnection;
-     public LoginForm loginForm;
-     public RegistrationForm registrationForm;
-     public ContactsWindow myContactWindows;
-     public ConversationWindow conversationWindow;
-     public int currentWindow; // 0 = Login , 1 =Registration, 2 = ContactsWindow, 3 = ConversationWindow
+     public LoginForm loginForm; // LOGIN 0
+     public RegistrationForm registrationForm; /// REGISTER 1
+     public ContactsWindow myContactWindows; // CONTACT 2
+     public ConversationWindow conversationWindow; // CHAT 3
+     public static int currentWindow; // 0 = Login , 1 =Registration, 2 = ContactsWindow, 3 = ConversationWindow
+
+     public static boolean alreadyDisplay;
 
      public User user;
 
      public ViewManagement(ServerConnection serverConnection) {
           this.serverConnection = serverConnection;
           this.currentWindow = 0;
-     }
-
-     public void setCurrentWindow(int currentWindow) {
-          this.currentWindow = currentWindow;
+          this.alreadyDisplay = false;
      }
 
      public void run () {
@@ -28,22 +29,35 @@ public class ViewManagement implements Runnable {
 
           do {
                switch (currentWindow) {
-                    case 0: // LOGIN
-                         loginForm.openLoginWindow();
-                         this.user = loginForm.user;
-                         break;
-                    case 1: // REGISTER
-                         this.user = registrationForm.user;
-                         serverConnection.addUser(user.getUserName(), user.getFirstName(), user.getLastName(), user.getMail(), user.getPassword(), "CLASSIC");//new
-                         break;
-                    case 2: // CONTACT
-                         initContact();
-                         break;
-                    case 3: // CHAT
-                         break;
-                    default:
-                         System.out.println("Invalid choice");
-                         break;
+                    case 0 -> { // LOGIN
+
+                         if (!alreadyDisplay) {
+                              loginForm.openLoginWindow();
+                              loginForm.closeLoginWindow();
+                              alreadyDisplay = true;
+                              //this.user = loginForm.user;
+                         }
+                    }
+                    case 1 -> { // REGISTER
+                         if (!alreadyDisplay) {
+                              this.user = registrationForm.user;
+                              serverConnection.addUser(user.getUserName(), user.getFirstName(), user.getLastName(), user.getMail(), user.getPassword(), "CLASSIC");
+                              alreadyDisplay = true;
+                         }
+                    }
+                    case 2 -> { // CONTACT
+                         if (!alreadyDisplay) {
+                              initContact();
+                              alreadyDisplay = true;
+                         }
+                    }
+                    case 3 -> { // CHAT
+
+                         if (!alreadyDisplay) {
+                              alreadyDisplay = true;
+                         }
+                    }
+                    default -> System.out.println("Invalid choice");
                }
           } while (true);
 
@@ -61,7 +75,13 @@ public class ViewManagement implements Runnable {
           myContactWindows.initComponents();
      }
 
-     public void setCurrentDisplay (int idOfWindowToDisplay) {
+     public void setAlreadyDisplay (boolean newSet) {
+          alreadyDisplay = newSet;
+     }
+
+     public static void setCurrentDisplay (int idOfWindowToDisplay) {
+          alreadyDisplay = false;
           currentWindow = idOfWindowToDisplay;
      }
+
 }
